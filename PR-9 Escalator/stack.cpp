308d28d98f20847e3af7fd0fdda9d7908b4389d4
Nisha <nishaparmar.0901@gmail.com>
2025-07-09 21:05:59 +0530

    Add files via upload

--- PR-9 Escalator/stack.cpp
+++ PR-9 Escalator/stack.cpp
@@ -47,7 +47,7 @@ class Stack{
             cout << "Stack is empty" << endl;
         } else {
             cout << "Stack elements are: ";
-            for (int i =0; i >= top; i--) {
+            for (int i =0; i <= top; i--) {
                 cout << arr[i] << " ";
             }
             cout << endl;
